d91b90bfe8edc0228825885d7a7f92c49129d40d
Jonnes Sena <jonnes.sena@globalhitss.com.br>
2020-02-04 11:20:16 -0300

    passou a funfar pelo sericeId

--- src/main/java/br/com/poc/poc_gateway/PocGatewayApplication.java
+++ src/main/java/br/com/poc/poc_gateway/PocGatewayApplication.java
@@ -1,9 +1,17 @@
 package br.com.poc.poc_gateway;
 
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
